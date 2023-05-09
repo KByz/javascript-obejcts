3f511cd4e4fd72cce88b50acd705c23e06ee8011
Kbyz <kira-byrne@live.nmit.ac.nz>
2023-05-10 09:52:33 +1200

    ex 1 dimensions

--- src/objects.js
+++ src/objects.js
@@ -1,1 +1,7 @@
-
+//Javascript Object Pair Coding - Property names and values
+//return total of all values
+function total(obj) {
+ 
+  return obj.height * obj.width * obj.depth
+}
+console.log(total({height: 5, width: 2, depth: 3}));
