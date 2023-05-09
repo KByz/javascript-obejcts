b67d8a0c780cd082f983d42d0d66a8544e10cb61
Kbyz <kira-byrne@live.nmit.ac.nz>
2023-05-10 10:22:16 +1200

    obj to array

--- src/objects.js
+++ src/objects.js
@@ -1,7 +1,19 @@
 //Javascript Object Pair Coding - Property names and values
 //return total of all values
+/*
 function total(obj) {
  
   return obj.height * obj.width * obj.depth
 }
-console.log(total({height: 5, width: 2, depth: 3}));
+console.log(total({height: 5, width: 2, depth: 3})); */
+
+//Convert obj to array of arrays
+/* function convert(obj) {
+  let arr = Object.entries(obj);
+  return arr;
+}
+
+console.log (convert({a: 1, b: 2, c: 3})); */
+
+
+
