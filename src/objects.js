146f1acc102c0a58dd5252546a47bc6a9733d72c
j-nowland <127156607+j-nowland@users.noreply.github.com>
2023-05-10 09:52:35 +1200

    volume

--- src/objects.js
+++ src/objects.js
@@ -1,2 +1,8 @@
-console.log("Hello")
+// volumeOfContainer({ width: 2, length: 5, height: 1}) // 10
+// need to pass an object in a function
+function total(obj) {
+    return obj.height * obj.width * obj.depth
+}
+console.log(total({ height: 2, width: 5, depth: 1 }))
+
 
